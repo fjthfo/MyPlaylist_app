82275a94704087c76421c249a4cb60d53afb0c3f
uryd <fjthfo@gmail.com>
2022-10-06 20:53:52 +0900

    하단 네비게이션 뷰 UI 구현

---
+++ app/src/main/java/com/example/myplaylist/recyclerviewmusiclist.java
@@ -0,0 +1,39 @@
+package com.example.myplaylist;
+
+//public class Music {
+//    private int imageResourceID;
+//    private String musicTitle;
+//    private String musicGrade;
+//
+//    public Music(int id, String title, String grade)
+//    {
+//        this.imageResourceID = id;
+//        this.musicTitle = title;
+//        this.musicGrade = grade;
+//    }
+//
+//    public int getImageResourceID() {
+//        return imageResourceID;
+//    }
+//
+//    public String getMusicTitle() {
+//        return musicTitle;
+//    }
+//
+//    public String getMusicGrade() {
+//        return musicGrade;
+//    }
+//
+//    public void setImageResourceID(int imageResourceID) {
+//        this.imageResourceID = imageResourceID;
+//    }
+//
+//    public void setMusicTitle(String musicTitle) {
+//        this.musicTitle = musicTitle;
+//    }
+//
+//    public void setMusicGrade(String musicGrade) {
+//        this.musicGrade = musicGrade;
+//    }
+//}
+
